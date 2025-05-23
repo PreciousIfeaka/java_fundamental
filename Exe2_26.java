d4c46131260e91f24cc2eae3c8ebf88f0b6ae586
PreciousIfeaka <preciousifeaka@gmail.com>
2025-05-23 16:20:09 +0100

    chore: completed third task

---
+++ Exe2_26.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Exe2_26 {
+  public static void main(String args[]) {
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("*****************");
+    System.out.println("This program compares two input ints and checks in 3x the first is a multiple of 2x the second");
+    System.out.println("*****************");
+
+    System.out.print("\nEnter the first integer: ");
+    int num1 = input.nextInt();
+
+    System.out.print("\nEnter the second integer: ");
+    int num2 = input.nextInt();
+
+    String result = (num1 * 3) % (num2 * 2) == 0 ? "Valid multiple" : "Invalid multiple";
+    System.out.println(result);
+  }
+}
