5452d1e4808bdeb4cecb4ce57d0719103874a34d
PreciousIfeaka <preciousifeaka@gmail.com>
2025-05-28 16:27:42 +0100

    feat: checkout app

--- credit_card_validator/Main.java
+++ credit_card_validator/Main.java
@@ -2,20 +2,30 @@ import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
+    String cardType;
+    String cardNumber;
+    int cardNumberLength;
+    String cardValidity;
 
-    System.out.print("Enter your card number: ");
-    String number = input.nextLine();
+    try (Scanner input = new Scanner(System.in)) {
+      System.out.print("Enter your card number: ");
+      String number = input.nextLine();
 
-    String cardNumberStr = number.replaceAll("\\s+", "");
+      String cardNumberStr = number.replaceAll("\\s+", "");
 
-    Validate cardValidator = new Validate(cardNumberStr);
+      Validate cardValidator = new Validate(cardNumberStr);
+
+      cardType = cardValidator.getCardType();
+      cardNumber = cardValidator.getCardNumber();
+      cardNumberLength = cardValidator.getCardNumber().length();
+      cardValidity = cardValidator.getCardValidity();
+    }
 
     System.out.println("******************************");
-    System.out.println("***Credit Card Type: " + cardValidator.getCardType());
-    System.out.println("***Credit Card Number: " + cardValidator.getCardNumber());
-    System.out.println("***Credit Card Digit Length: " + cardValidator.getCardNumber().length());
-    System.out.println("***Credit Card Validity Status: " + cardValidator.getCardValidity());
+    System.out.println("***Credit Card Type: " + cardType);
+    System.out.println("***Credit Card Number: " + cardNumber);
+    System.out.println("***Credit Card Digit Length: " + cardNumberLength);
+    System.out.println("***Credit Card Validity Status: " + cardValidity);
     System.out.println("******************************");
   }
 }
